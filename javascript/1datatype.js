7068e6fb66c49a75a951c0f3f480438b0ad5a215
aakashdeveloper <ahanda205@gmail.com>
2024-05-04 13:00:39 +0530

    javascript

---
+++ javascript/1datatype.js
@@ -0,0 +1,99 @@
+es5 > Ecma Script 5
+es6 > Ecma script 6
+
+Datatypes
+String > "hii"  'hii'  "10" `hi10`  'true'
+Number > 3  435345345   345345.4345 .43
+Boolean > true false
+
+//es5
+var a = "Hiii"
+var b = 10
+var c = true
+
+typeof(a)
+'string'
+typeof(b)
+'number'
+typeof(c)
+'boolean'
+var d = "10"
+typeof(d)
+'string'
+var e = `true`
+typeof(e)
+'string'
+var a = 10
+var b = 20
+
+undefined
+a+b
+30
+a-b
+-10
+b-a
+10
+a*b
+200
+a/b
+0.5
+b%a
+0
+4%2
+0
+4%3
+1 
+
+var a = "Hii"
+var b = "Javascript"
+'HiiJavascript'
+
+var c= 10
+a+c
+'Hii10'
+a-b
+NaN (Not a number)
+a*b
+NaN
+a/b
+NaN
+
+
+string + string = string
+string + number = string
+number + string = string
+number + number = number
+
+
+"10"+20+30
+"1020"+30
+'102030'
+
+10+"20"+30
+"1020"+30
+'102030'
+
+10+20+"30"
+30+"30"
+'3030'
+
+
+"10"+20+30-1
+'102029'
+10+"20"+30-1
+'102029'
+10+20+"30"-1
+'3029'
+
+"hi"-1
+NaN
+"10"-1
+9
+"10a"-1
+NaN
+"30"*2
+60
+2*"23g"
+NaN
+"10"+1
+'101'
---
+++ javascript/2datatype.js
@@ -0,0 +1,53 @@
+true is equal to 1
+false is equal to 0
+
+true + true
+2
+true+false
+1
+false+false
+0
+
+true + 1
+2
+10/false
+Infinity
+"true"+5
+'true5'
+"true"+"true"
+'truetrue'
+true- true
+0
+
+var a = "10"
+var b = "20"
+undefined
+a+b
+'1020'
+parseInt(a)
+10
+parseInt(b)
+20
+parseInt(a)+parseInt(b)
+30
+var a = "10.11"
+var b = "20"
+undefined
+parseInt(a)+parseInt(b)
+30
+parseFloat(a)+parseInt(b)
+30.11
+var a = "2a105b1"
+undefined
+parseInt(a)
+2
+Number(a)+Number(b)
+NaN
+var a = "105"
+undefined
+Number(a)+Number(b)
+125
+var a = "2a105b1"
+undefined
+Number(a)
+NaN
