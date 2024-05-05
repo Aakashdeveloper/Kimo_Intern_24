e5b700a5d51c589cdb8db7750511d1de8aa42994
Bhumika Bajpai <bhumikabajpai@Bhumikas-MacBook-Air.local>
2024-05-05 11:48:24 +0530

    js

---
+++ javascript/3condition.js
@@ -0,0 +1,63 @@
+//all the conditional operator will return boolean
+ = // assignment
+ == // compare the data
+ === // compare the data and datatype
+
+
+var a = 10   //assignment
+var b = "10"
+
+a == b  //compare the data
+true
+
+a === b // compare the data and datatype
+false
+
+var c = 20
+a == c
+false
+
+
+var a = 10
+var b = 5
+
+false
+a != b
+true
+a <= b
+false
+a >= b
+true
+a => b (not to use)
+
+
+
+negation
+
+var a = true
+
+!a
+false
+var a = false
+undefined
+!a
+true
+var a = 1
+undefined
+!a
+false
+
+var a = "Hii"
+undefined
+!a
+false
+var a = -1
+undefined
+!a
+false
+
+
+truthy
+> Any number except 0 , any string , true
+falsy
+> 0, false,null, undefined
---
+++ javascript/5letconst.js
@@ -0,0 +1,4 @@
+var > we can redeclare and reassign
+let > we cannot redeclare but can reassign
+const> we cannot redeclare nor reassign
+
